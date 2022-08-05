e28f0d037189da1c626a39972ee4daafe93e9b58
IzanDuesoLopez <izandueso@gmail.com>
2022-08-05 09:37:25 +0200

    Pruebas unitarias rectangulos

--- Junit09_Geometria/src/main/java/test/GeometriaTest.java
+++ Junit09_Geometria/src/main/java/test/GeometriaTest.java
@@ -4,11 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import JUnit.Junit09_Geometria.dto.Geometria;
+
 class GeometriaTest {
 
+	Geometria geometria = new Geometria();
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testAreaRectangulo() {
+		int entrada = geometria.arearectangulo(5, 5);
+		int salida = 25;
+		assertEquals(salida, entrada);
+	}
+	
+	@Test
+	public void testAreaPentagono() {
+		int entrada = geometria.areapentagono(3, 2);
+		int salida = 3;
+		assertEquals(salida, entrada);
+	}
+	
+	@Test
+	public void testAreaRombo() {
+		int entrada = geometria.arearombo(4, 5);
+		int salida = 10;
+		assertEquals(salida, entrada);
 	}
 
 }
