fd95fc3fafdf165ef6b12630e6c8ec627e3597b2
IzanDuesoLopez <izandueso@gmail.com>
2022-08-05 09:50:37 +0200

    Before refactor

--- Junit09_Geometria/src/main/java/test/GeometriaTest.java
+++ Junit09_Geometria/src/main/java/test/GeometriaTest.java
@@ -2,51 +2,60 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import JUnit.Junit09_Geometria.dto.Geometria;
 
 class GeometriaTest {
+
+	Geometria geometria;
 	
-	Geometria geometria = new Geometria();
-	
+	@BeforeEach
+	public void before() {
+		geometria = new Geometria();
+	}
+
 	@Test
 	public void testAreaCuadrado() {
 		System.out.println("testAreaCuadrado()");
 		int resul = geometria.areacuadrado(2);
 		int esper = 4;
-		assertEquals(esper,resul);
+		assertEquals(esper, resul);
 	}
-	
+
 	@Test
 	public void testAreaCirculo() {
 		System.out.println("testAreaCirculo()");
 		double resul = geometria.areaCirculo(3);
 		double esper = 28.2744;
-		assertEquals(esper,resul);
+		assertEquals(esper, resul);
 	}
-	
+
 	@Test
 	public void testAreaTriangulo() {
 		System.out.println("testAreaTriangulo()");
-		int resul = geometria.areatriangulo(2,3);
+		int resul = geometria.areatriangulo(2, 3);
 		int esper = 3;
-		assertEquals(esper,resul);
+		assertEquals(esper, resul);
 
-@Test
+	}
+
+	@Test
 	public void testAreaRectangulo() {
 		int entrada = geometria.arearectangulo(5, 5);
 		int salida = 25;
 		assertEquals(salida, entrada);
 	}
-	
+
 	@Test
 	public void testAreaPentagono() {
 		int entrada = geometria.areapentagono(3, 2);
 		int salida = 3;
 		assertEquals(salida, entrada);
 	}
-	
+
 	@Test
 	public void testAreaRombo() {
 		int entrada = geometria.arearombo(4, 5);
