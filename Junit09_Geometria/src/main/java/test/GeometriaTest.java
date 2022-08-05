ddd29039d6dcc67d2e834796250665997c3af6f0
daniflorialopez <daniflorialopez@gmail.com>
2022-08-05 10:08:46 +0200

    Unit Tests

--- Junit09_Geometria/src/main/java/test/GeometriaTest.java
+++ Junit09_Geometria/src/main/java/test/GeometriaTest.java
@@ -2,13 +2,53 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import JUnit.Junit09_Geometria.dto.Geometria;
 
 class GeometriaTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	Geometria geometria = new Geometria();
+	
+	private static Stream<Arguments> getFormatFixture() {
+		return Stream.of(
+				Arguments.of(1, "cuadrado"),
+				Arguments.of(2, "Circulo"),
+				Arguments.of(3, "Triangulo"),
+				Arguments.of(4, "Rectangulo"),
+				Arguments.of(5, "Pentagono"),
+				Arguments.of(6, "Rombo"),
+				Arguments.of(7, "Romboide"),
+				Arguments.of(8, "Trapecio"),
+				Arguments.of(9, "Default"));
 	}
-
+	
+	//Test 7
+	@Test
+	public void testAreaRomboide() {
+		int result = geometria.arearomboide(2, 3);
+		int expected = 6;
+		assertEquals(expected, result);
+	}
+	
+	//Test 8
+	@Test
+	public void testAreaTrapecio() {
+		int result = geometria.areatrapecio(5, 3, 4);
+		int expected = 16;
+		assertEquals(expected, result);
+	}
+	
+	//Test 9
+	@ParameterizedTest
+	@MethodSource("getFormatFixture")
+	public void testFigura(int a, String b) {
+		String result = geometria.figura(a);
+		assertEquals(b, result);
+	}
 }
