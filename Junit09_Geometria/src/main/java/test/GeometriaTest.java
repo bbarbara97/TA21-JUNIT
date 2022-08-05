1ada5112f92eaddef0202040024cbbcf996508c7
IzanDuesoLopez <izandueso@gmail.com>
2022-08-05 09:23:35 +0200

    GeometriaTest

---
+++ Junit09_Geometria/src/main/java/test/GeometriaTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GeometriaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
